9d0508cda4959ed2c11ef94c89a051d62707c7e8
frank <frankwu@gmx.de>
2017-12-14 13:21:17 +0100

    updated build.sh from main (pack+basic checks) updated README with instructions how-to-use

--- build.sh
+++ build.sh
@@ -5,10 +5,13 @@ then
   exit 1;
 fi
 
+if [[ -z "$(which arm-linux-gnueabihf-gcc)" ]];then echo "erst gcc-arm-linux-gnueabihf installieren";exit 1;fi
+if [[ -z "$(which mkimage)" ]];then echo "erst u-boot-tools installieren";exit 1;fi
+
 if [[ -d drivers ]];
 then
   action=$1
-  LANG=C
+
   #git pull
   #git reset --hard v4.14
   export INSTALL_MOD_PATH=$(dirname $(pwd))/mod/;export ARCH=arm;export CROSS_COMPILE=arm-linux-gnueabihf-
@@ -46,24 +49,56 @@ then
   if [[ -z "$action" ]];
   then
  #   set -x
-    exec 3> >(tee build.log)
 #    make --debug && make modules_install
-    make 2>&3 && make modules_install 2>&3
+    make && make modules_install
     ret=$?
 #    set +x
-    exec 3>&-
     if [[ $ret == 0 ]];
     then
       cat arch/arm/boot/zImage arch/arm/boot/dts/mt7623n-bananapi-bpi-r2.dtb > arch/arm/boot/zImage-dtb
       mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel $(git describe --tags)" -d arch/arm/boot/zImage-dtb ./uImage
-      read -p "Press [enter] to copy data to SD-Card..."
-      if  [[ -d /media/$USER/BPI-BOOT ]];
-      then
-        cp ./uImage /media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
-        sudo cp -r ../mod/lib/modules /media/$USER/BPI-ROOT/lib/
-        sync
+      echo "==========================================="
+      echo -e "1) pack\n2) install to SD-Card"
+      read -n1 -p "choice [12]:" choice
+      echo
+      if [[ "$choice" == "1" ]]; then
+        mkdir -p ../SD
+        echo "cleanup..."
+        rm -r ../SD/BPI-BOOT/
+        rm -r ../SD/BPI-ROOT/
+        mkdir -p ../SD/BPI-BOOT/bananapi/bpi-r2/linux/
+        mkdir -p ../SD/BPI-ROOT/lib/modules
+        echo "copy..."
+        cp ./uImage ../SD/BPI-BOOT/bananapi/bpi-r2/linux/uImage
+        cp -r ../mod/lib/modules ../SD/BPI-ROOT/lib/
+        echo "pack..."
+        kernver=$(make kernelversion)
+        gitbranch=$(git rev-parse --abbrev-ref HEAD)
+        olddir=$(pwd)
+        cd ../SD
+        fname=bpi-r2_${kernver}_${gitbranch}.tar.gz
+        tar -czf $fname BPI-BOOT BPI-ROOT
+        ls -lh $(pwd)"/"$fname
+        cd $olddir
+      elif [[ "$choice" == "2" ]];then
+        read -p "Press [enter] to copy data to SD-Card..."
+        if  [[ -d /media/$USER/BPI-BOOT ]];
+        then
+		  kernelfile=/media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
+          if [[ -e $kernelfile ]];then
+		    echo "backup of kernel: $kernelfile.bak"
+		    cp $kernelfile $kernelfile.bak
+		  fi
+		  echo "copy new kernel"
+          cp ./uImage /media/$USER/BPI-BOOT/bananapi/bpi-r2/linux/uImage
+		  echo "copy modules (root needed because of ext-fs permission)"
+          sudo cp -r ../mod/lib/modules /media/$USER/BPI-ROOT/lib/
+          sync
+        else
+          echo "SD-Card not found!"
+        fi
       else
-        echo "SD-Card not found!"
+        echo "wrong option: $choice"
       fi
     fi
   fi
