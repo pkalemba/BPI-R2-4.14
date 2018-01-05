080cf80cc1604fcd6e9ceba99d45b741b37058b5
frank <frankwu@gmx.de>
2018-01-05 20:39:47 +0100

    disabled ump, fixed __GFP_REPEAT

--- mali/build.sh
+++ mali/build.sh
@@ -4,17 +4,22 @@ logfile=build.log
 #echo $kerneldir
 bld=debug
 #bld=release
+ump=0
+#ump=1
 
 export ARCH=arm;export CROSS_COMPILE=arm-linux-gnueabihf-
 cd src/devicedrv
-
-#first build ump-driver
-cd ump
-ret=1
-KDIR=$kerneldir CONFIG=default BUILD=$bld make 2> >( tee "$logfile" ) && ret=0
-if [[ -z $ret ]];then
-  echo "building mali-driver"
-  cd ../mali
+ret=0
+if [[ $ump -eq 1 ]];then
+  #first build ump-driver
+  cd ump
   ret=1
-  KDIR=$kerneldir USING_UMP=1 BUILD=$bld make 2> >( tee "$logfile" ) && ret=0
+  KDIR=$kerneldir CONFIG=default BUILD=$bld make 2> >( tee "$logfile" ) && ret=0
+  cd ..
+fi
+if [[ $ret -eq 0 ]];then
+  echo "building mali-driver"
+  cd mali
+  ret=1
+  KDIR=$kerneldir USING_UMP=$ump BUILD=$bld make 2> >( tee "$logfile" ) && ret=0
 fi
