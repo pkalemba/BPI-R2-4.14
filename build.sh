e331e6fbeb026fd697f7616c611c9a9a5f5ffa36
frank <frankwu@gmx.de>
2018-03-18 18:04:36 +0100

    [build.sh] removed modules_install (4.9 makes no modules) and added mkimage

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export ARCH=arm
-export INSTALL_MOD_PATH=$(pwd)/mod/
-mkdir -p $INSTALL_MOD_PATH
+#export INSTALL_MOD_PATH=$(pwd)/mod/
+#mkdir -p $INSTALL_MOD_PATH
 
 case $1 in
 
@@ -14,6 +14,10 @@ case $1 in
   ;;
 *)
   make
-  make modules_install
+  if [[ $? -eq 0 ]];then
+    cat arch/arm/boot/zImage arch/arm/boot/dts/mt7623n-bananapi-bpi-r2.dtb > arch/arm/boot/zImage-dtb
+    mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel 4.9" -d arch/arm/boot/zImage-dtb ./uImage
+  fi
+#  make modules_install
   ;;
 esac
