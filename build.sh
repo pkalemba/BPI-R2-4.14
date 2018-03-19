7716837a6f5da7e23026999d62280e371cb1f0d1
frank <frankwu@gmx.de>
2018-03-19 19:42:47 +0100

    optimized build.sh (using more threads)

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export ARCH=arm
+LANG=C
+CFLAGS=-j$(grep ^processor /proc/cpuinfo  | wc -l)
 #export INSTALL_MOD_PATH=$(pwd)/mod/
 #mkdir -p $INSTALL_MOD_PATH
 
@@ -15,7 +17,7 @@ case $1 in
   make menuconfig
   ;;
 *)
-  make
+  make ${CFLAGS}
   if [[ $? -eq 0 ]];then
     cat arch/arm/boot/zImage arch/arm/boot/dts/mt7623n-bananapi-bpi-r2.dtb > arch/arm/boot/zImage-dtb
     mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel 4.9" -d arch/arm/boot/zImage-dtb ./uImage
